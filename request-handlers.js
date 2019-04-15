2b9e82c28a1b481b755de67fd29412892ca6dbb4
UncleJerry <jared.myhrberg@gmail.com>
2019-04-15 05:43:35 -0700

    test png showing at /show

--- request-handlers.js
+++ request-handlers.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring');
+const fs = require('fs');
 
 function start(response, postData) {
   console.log('Request handle \'start\' was called.');
@@ -25,12 +26,18 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log('Request handler \'upload\' was called.');
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('You\'ve sent ' + postData);
-  querystring.parse((postData).text);
+  response.write('You\'ve sent the text ' + querystring.parse(postData).text);
   response.end();
+}
+
+function show(response) {
+  console.log('Request handler show was called.');
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  fs.createReadStream('./tmp/test.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
 
