d3d0cfddc1fbe93ee448b5d373f68859f4e5bb77
UncleJerry <jared.myhrberg@gmail.com>
2019-04-15 06:16:14 -0700

    upload image, err call back not a function

--- request-handlers.js
+++ request-handlers.js
@@ -1,21 +1,23 @@
 const querystring = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
   console.log('Request handle \'start\' was called.');
  
   var body = '<html>' +
 '<head>' +
-'<meta http-equiv="Content-Type" content="text/html; ' +
-'charset=UTF-8" />' +
+'<meta http-equiv="Content-Type" ' +
+'content="text/html; charset=UTF-8" />' +
 '</head>' +
 '<body>' +
- '<form action="/upload" method="post">' +
- '<textarea name="text" rows="20" cols="60"></textarea>' +
- '<input type="submit" value="Submit text" />' +
- '</form>' +
- '</body>' +
- '</html>';
+'<form action="/upload" enctype="multipart/form-data" ' +
+'method="post">' +
+'<input type="file" name="upload" multiple="multiple">' +
+'<input type="submit" value="Upload file" />' +
+'</form>' +
+'</body>' +
+'</html>';
 
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(body);
@@ -23,11 +25,25 @@ function start(response, postData) {
 }
 
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log('Request handler \'upload\' was called.');
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('You\'ve sent the text ' + querystring.parse(postData).text);
-  response.end();
+
+  const form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, './tpm/test.png', (error) => {
+      if(error) {
+        fs.unlink('./tmp/test.png');
+        fs.rename(files.upload.path, './tmp/test.png');
+      }
+    });
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('received image <br/>');
+    response.write('<img src=\'/show\' />\'');
+    response.end();
+  });
 }
 
 function show(response) {
